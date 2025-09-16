0240d7cb216d57c69ec3b17fc09fc01b736d024f
HiTomorrowGW <kidestory@hanmail.net>
2025-09-16 18:14:09 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,26 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
     let footerPath = 'includes/footer.html';
 
     const currentPath = window.location.pathname;
-    if (currentPath.includes('/rooms/') || currentPath.endsWith('/rooms.html') || currentPath.endsWith('/room.html') || currentPath.endsWith('/contact.html')) {
+    // Adjust paths for pages that are not in the root directory
+    if (currentPath.includes('/static_rooms/') || currentPath.endsWith('/rooms.html') || currentPath.endsWith('/contact.html')) {
         headerPath = '../includes/header.html';
         footerPath = '../includes/footer.html';
     }
 
-    fetch(headerPath)
-        .then(response => response.text())
-        .then(data => {
-            const headerPlaceholder = document.getElementById('header-placeholder');
-            headerPlaceholder.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading header:', error));
+    const headerPlaceholder = document.getElementById('header-placeholder');
+    if (headerPlaceholder) { // Only load if placeholder exists
+        fetch(headerPath)
+            .then(response => response.text())
+            .then(data => {
+                headerPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading header:', error));
+    }
 
-    // Load footer
-    fetch(footerPath)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
+    const footerPlaceholder = document.getElementById('footer-placeholder');
+    if (footerPlaceholder) { // Only load if placeholder exists
+        fetch(footerPath)
+            .then(response => response.text())
+            .then(data => {
+                footerPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error('Error loading footer:', error));
+    }
 
     // Handle image modal
     const imageModal = document.getElementById('imageModal');
